ffbe053af3eead05c95c926580650567883c52e8
lacj2000 <luizjuniordeveloper@gmail.com>
2025-08-19 10:28:54 -0300

    fix: add explicit status code to root endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Hello World!'}
